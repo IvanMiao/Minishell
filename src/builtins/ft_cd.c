a97b54a385a9bb8fc489990901db33b845781880
Ivan Miao <reneisaipa@gmail.com>
2025-03-27 03:09:05 +0100

    a better test for cd

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ymiao <ymiao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 19:25:44 by ymiao             #+#    #+#             */
-/*   Updated: 2025/03/27 01:41:56 by ymiao            ###   ########.fr       */
+/*   Updated: 2025/03/27 03:08:11 by ymiao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ int	main(int ac, char **av)
 	{
 		printf("minishell: %s", cwd);
 		path = readline("$ ");
-		if (ft_cd(path) == 1)
-			printf("error\n");
+		if (strncmp(path, "cd ", 3) == 0)
+			ft_cd(path+3);
 		getcwd(cwd, sizeof(cwd));
 	}
 	return (0);
