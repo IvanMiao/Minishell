9ffec22f5f0c909f220c14aea6eec73ba3e00623
Caspar Gerner <cgerner@bess-f4r4s2.clusters.42paris.fr>
2025-04-02 17:42:32 +0200

    Modifs des valeurs de return

--- src/parsing/check_command.c
+++ src/parsing/check_command.c
@@ -6,7 +6,7 @@
 /*   By: cgerner <cgerner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 12:33:57 by cgerner           #+#    #+#             */
-/*   Updated: 2025/04/02 16:54:57 by cgerner          ###   ########.fr       */
+/*   Updated: 2025/04/02 17:11:03 by cgerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	check_command(t_token *token)
 		{
 			if (!last || !token->next || token->next->type == PIPE)
 				return (printf(
-						"bash: syntax error near unexpected token `|'\n"), 0);
+						"bash: syntax error near unexpected token `|'\n"), 2);
 		}
 		if (token->type == R_IN || token->type == R_OUT
 			|| token->type == R_DELIMITER || token->type == R_REDIRECTION
@@ -32,7 +32,7 @@ int	check_command(t_token *token)
 		{
 			if (!last || !token->next)
 				return (printf
-					("bash: syntax error near unexpected token `newline'\n"), 0);
+					("bash: syntax error near unexpected token `newline'\n"), 2);
 		}
 		last = token;
 		token = token->next;
@@ -50,10 +50,10 @@ int	check_command_in(t_token *token)
 		{
 			if (!last || !token->next || token->next->type == R_IN)
 				return (printf
-					("bash: syntax error near unexpected token `<'\n"), 0);
+					("bash: syntax error near unexpected token `<'\n"), 2);
 			if (!last || !token->next || token->next->type == R_DELIMITER)
 				return (printf
-					("bash: syntax error near unexpected token `<<'\n"), 0);
+					("bash: syntax error near unexpected token `<<'\n"), 2);
 		}
 		last = token;
 		token = token->next;
@@ -71,10 +71,10 @@ int	check_command_out(t_token *token)
 		{
 			if (!last || !token->next || token->next->type == R_OUT)
 				return (printf
-					("bash: syntax error near unexpected token `>'\n"), 0);
+					("bash: syntax error near unexpected token `>'\n"), 2);
 			if (!last || !token->next || token->next->type == R_REDIRECTION)
 				return (printf
-					("bash: syntax error near unexpected token `>>'\n"), 0);
+					("bash: syntax error near unexpected token `>>'\n"), 2);
 		}
 		last = token;
 		token = token->next;
@@ -92,10 +92,10 @@ int	check_command_delimiter(t_token *token)
 		{
 			if (!last || !token->next || token->next->type == R_DELIMITER)
 				return (printf
-					("bash: syntax error near unexpected token `<<'\n"), 0);
+					("bash: syntax error near unexpected token `<<'\n"), 2);
 			if (!last || !token->next || token->next->type == R_IN)
 				return (printf
-					("bash: syntax error near unexpected token `<'\n"), 0);
+					("bash: syntax error near unexpected token `<'\n"), 2);
 		}
 		last = token;
 		token = token->next;
@@ -113,10 +113,10 @@ int	check_command_redirection(t_token *token)
 		{
 			if (!last || !token->next || token->next->type == R_REDIRECTION)
 				return (printf
-					("bash: syntax error near unexpected token `>>'\n"), 0);
+					("bash: syntax error near unexpected token `>>'\n"), 2);
 			if (!last || !token->next || token->next->type == R_OUT)
 				return (printf
-					("bash: syntax error near unexpected token `>'\n"), 0);
+					("bash: syntax error near unexpected token `>'\n"), 2);
 		}
 		last = token;
 		token = token->next;
